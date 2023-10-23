19039fe065d172abfea7c5b6df58ec8a55f07142
fadlan02 <fadlan.rizki@nexsoft.co.id>
2023-10-23 15:59:34 +0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React test
         </a>
         <div className="test"></div>
       </header>
